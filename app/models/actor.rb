f6112c49a53d7eb144a138d2297d91b82a3800b4
mborreros <mlbsindia@gmail.com>
2022-02-24 15:26:53 -0500

    commit tree 96764020f90c2c8057836ad5bcf551f3c6f9c98b

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+  "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
   
+    list_of_characters = self.characters
+    array_of_roles = []
+
+    list_of_characters.each do |character|
+      array_of_roles << "#{character.name} - #{character.show.name}"
+    end
+    array_of_roles
+    
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+
+    array_of_actors = self.actors
+    list_of_actors = []
+
+    array_of_actors.each do |actor|
+
+      list_of_actors << "#{actor.first_name} #{actor.last_name}"
+    end
+
+    list_of_actors
+  end
+
 end
